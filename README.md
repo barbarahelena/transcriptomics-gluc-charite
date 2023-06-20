188e9b997ca14378523a6ff9590c46beb49a9a7b
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2023-06-20 14:34:48 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# transcriptomics-gluc-charite
+# Transcriptomics glucagon HAEC experiments
+
+## Introduction
+We performed differential gene expression (DGE) analyses to assess the effects of glucagon on HAECs. The RNA sequencing was performed by Azenta using Illumina NovaSeq, PE 2x150. There were 3 replicates per condition (10 nM glucagon and control), originating from three separate experiments. [add more details about experiments]
+
+## Processing RNA seq data (Azenta report)
+Sequence reads were trimmed to remove possible adapter sequences and nucleotides with poor quality using Trimmomatic v.0.36. The trimmed reads were mapped to the Homo sapiens GRCh38 reference genome available on ENSEMBL using the STAR aligner v.2.5.2b. The STAR aligner is a splice aligner that detects splice junctions and incorporates them to help align the entire read sequences. BAM files were generated as a result of this step.
+
+Unique gene hit counts were calculated by using featureCounts from the Subread package v.1.5.2. The hit counts were summarized and reported using the gene_id feature in the annotation file. Only unique reads that fell within exon regions were counted. If a strand-specific library preparation was performed, the reads were strand-specifically counted.
+
+## DGE analyses
+Genes with less than 5 counts across samples were filtered out. We used DESeq2 (v.1.36.0) to assess the differentially expressed genes between control and glucagon-stimulated samples (see `deseq2.R`). For these analyses, the formula Gene ~ Condition was used, with a Wald test and parametric fit. Genes with a P-value <0.01 were considered as differentially expressed between control and glucagon conditions. Gene counts were normalized using DESeq2’s median of ratios method to account for sequencing depth and RNA composition. 
+
+## Plots
+See the `plots.R` script. The following plots were drawn:
+- Volcano plot: drawn using ggplot2 (v3.4.0). 
+- Heatmap: After zero mean, unit variance scaling of the normalized gene counts, a heatmap with differentially expressed genes was drawn using the ComplexHeatmap package (v2.12.1). The dendrogram in this plot was constructed with Ward’s method.
+- Boxplots
+
+## Pathway analyses
+Pathway analyses were performed using GO and KEGG. There were no significantly expressed pathways (padj < 0.1). One of the KEGG pathways with the lowest p-values was the hsa01522 "Endocrine resistance" pathway. That pathway, although not significant, was visualized. Plots can be found in the hsa01522 folder, in the pdf folder of the results.
+
+## Data availability
+Data will be made available on ENA. As soon as the data are available, the repository link will be made available here.
---
+++ scripts/deseq2.R
@@ -0,0 +1,110 @@
+## DESeq2 analysis
+
+## libraries
+library(DESeq2)
+library(dplyr)
+library(stringr)
+
+# Pending installations
+# updateR::updateR()
+# BiocManager::install("BiocParallel")
+# install.packages("locfit")
+# BiocManager::install(c("gage","GO.db","AnnotationDbi","org.Hs.eg.db"))
+
+## Check summary stats
+summarystats <- read.csv('data/01_analysis/stats/star/summary_mapping_stats.csv')
+summarystats
+head(summarystats)
+sampleno <- nrow(summarystats)
+print(str_c("Mean total reads for ", sampleno, " samples was: ", format(mean(summarystats$Total.Reads), 10), 
+            " with an SD of: ", format(sd(summarystats$Total.Reads), 10), " of which ", 
+            format((mean(summarystats$Total.Mapped.Reads)/mean(summarystats$Total.Reads)) * 100, 1),
+            "% were mapped."
+            ))
+
+## Open raw count file
+rawCounts <- read.csv('data/01_analysis/DEG/deseq2/Control-vs-Stimulation/counts/raw_counts.csv')
+rawCounts$Gene.ID <- rawCounts$X
+rawCounts$X <- NULL
+print(str_c("Mean total counts for ", sampleno, " samples was: ", mean(colSums(rawCounts[,-1])), 
+            " with an SD of: ", sd(colSums(rawCounts[,-1]))))
+
+## Open sample mapping
+sampleData <- read.delim("data/01_analysis/DEG/deseq2/Control-vs-Stimulation/testCondition.txt")
+sampleData$batch <- NULL
+sampleData <- sampleData %>% mutate(SampleName = str_remove(SampleID, "10nM-"),
+                                    condition = as.factor(condition),
+                                    condition = forcats::fct_recode(condition,
+                                                                    "Treatment" = "Control",
+                                                            "Control" = "Stimulation"),
+                                    condition = forcats::fct_relevel(condition, "Control", after = 0L),
+                                    SampleID = case_when(
+                                        str_detect(SampleID, "10nM") ~ str_c("X", str_replace_all(SampleID, "-", ".")),
+                                        str_detect(SampleID, "Ctrl") ~ str_replace_all(SampleID, "-", ".")
+                                    ),
+                                    Run = "1")
+head(sampleData)
+levels(sampleData$condition)
+rownames(sampleData) <- sampleData$SampleID
+
+# Also save a copy for later
+sampleData_v2 <- sampleData
+
+# Convert count data to a matrix of appropriate form that DEseq2 can read
+geneID <- rawCounts$Gene.ID
+sampleIndex <- !grepl("Gene", colnames(rawCounts))
+rawCounts <- as.matrix(rawCounts[,sampleIndex])
+rownames(rawCounts) <- geneID
+head(rawCounts)
+
+# Put the columns of the count data in the same order as rows names of the sample mapping, then make sure it worked
+rawCounts <- rawCounts[,unique(rownames(sampleData))]
+all(colnames(rawCounts) == rownames(sampleData))
+
+# Create the DEseq2DataSet object
+deseq2Data <- DESeqDataSetFromMatrix(countData=rawCounts, colData=sampleData, design= ~ condition)
+
+# Prefiltering data (low counts)
+dim(deseq2Data)
+dim(deseq2Data[rowSums(counts(deseq2Data)) > 5, ])
+deseq2Data <- deseq2Data[rowSums(counts(deseq2Data)) > 5, ]
+
+# Run pipeline for differential expression steps 
+deseq2Data <- DESeq(deseq2Data)
+
+# Extract differential expression results
+deseq2Results <- results(deseq2Data, contrast=c("condition", "Treatment", "Control"),
+                         alpha = 0.1)
+deseq2Results$ENSEMBL <- rownames(deseq2Results)
+
+summary(deseq2Results)
+
+# out of 21475 with nonzero total read count
+# adjusted p-value < 0.1
+# LFC > 0 (up)       : 0, 0%
+# LFC < 0 (down)     : 0, 0%
+# outliers [1]       : 0, 0%
+# low counts [2]     : 0, 0%
+# (mean count < 1)
+# [1] see 'cooksCutoff' argument of ?results
+# [2] see 'independentFiltering' argument of ?results
+
+plotMA(deseq2Results)
+
+deseq2Results[which(deseq2Results$pvalue < 0.1),]
+
+library(EnsDb.Hsapiens.v86)
+deseq2Results$symbol <- mapIds(EnsDb.Hsapiens.v86, keys=deseq2Results$ENSEMBL,
+                               column="SYMBOL", keytype="GENEID", multiVals="first")
+
+counts <- as.data.frame(counts(estimateSizeFactors(deseq2Data), normalized=TRUE))
+counts$ENSEMBL <- rownames(counts)
+
+deseq2Results <- dplyr::right_join(counts, as.data.frame(deseq2Results), by = "ENSEMBL")
+
+# Save files
+# DESeq2 results object
+head(deseq2Results)
+deseq2Results %>% dplyr::filter(pvalue < 0.01)
+write.csv2(deseq2Results, "data/230620_deseq2results.csv")
+head(deseq2Results)
